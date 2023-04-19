2eacd56df6ed0205145a887217b008fd62fa83c4
kpkusumanjali2000 <kusumanjalikp1995@gmail.com>
2023-04-19 20:05:16 +0530

    feat: :zap: folder changes

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
-import Home from './components/home';
+import Home from './pages/home';
 function App() {
   return (
     <div>
