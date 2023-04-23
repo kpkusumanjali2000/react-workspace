bb506443b2a422c675f4b4a643809963f65f21f9
kpkusumanjali2000 <kusumanjalikp1995@gmail.com>
2023-04-23 19:28:19 +0530

    feat: :sparkles: added about session

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -3,12 +3,15 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import Home from './pages/home';
+import About from './pages/about';
+
 function App() {
   return (
     <div>
       <Router>
         <Navbar/>
         <Home/>
+        <About/>
         <Footer/>
       </Router>
     </div>
