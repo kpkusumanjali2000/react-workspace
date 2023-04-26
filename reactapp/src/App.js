2055347743270d52cb900fda71db68fb7bedfe2e
kpkusumanjali2000 <kusumanjalikp1995@gmail.com>
2023-04-26 19:35:56 +0530

    feat: :bookmark: added contact section

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -4,8 +4,8 @@ import Navbar from './components/navbar';
 import Footer from './components/footer';
 import Home from './pages/home';
 import About from './pages/about';
-import Contact from './pages/contact';
 import Menu from './pages/menu';
+import Contact from './pages/contact';
 
 function App() {
   return (
